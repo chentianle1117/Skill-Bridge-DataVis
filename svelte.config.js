4119fc29499a6b88faaad55c52b0d16985996786
chentianle1117 <79094173+chentianle1117@users.noreply.github.com>
2025-01-09 17:11:06 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -26,4 +26,16 @@ const config = {
 	},
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
